0982118fb43a4d852702ba775fdd8607339a1aaf
Johannes / EinJojo <62117829+EinJOJO@users.noreply.github.com>
2021-12-02 18:19:44 +0100

    Added Constructors

--- src/main/java/me/einjojo/survivalengine/object/Team.java
+++ src/main/java/me/einjojo/survivalengine/object/Team.java
@@ -1,23 +1,57 @@
 package me.einjojo.survivalengine.object;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Team implements ConfigurationSerializable {
 
-    private int id;
+    private final UUID id;
+    private final List<UUID> members;
+
+
     private String name;
-    private Player Owner;
-    private SurvivalPlayer[] members;
+    private UUID owner;
     private Location baseLocation;
 
 
-    public int getId() {
+    public Team(String name, UUID owner)
+    {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.owner = owner;
+        this.members = new ArrayList<>();
+        this.baseLocation = null;
+    }
+
+    public Team(UUID id, List<UUID> members, String name, UUID owner, Location baseLocation) {
+        this.id = id;
+        this.members = members;
+        this.name = name;
+        this.owner = owner;
+        this.baseLocation = baseLocation;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
         return id;
+    }
+
+
+
+    public void setOwner(UUID owner) {
+        this.owner = owner;
+    }
+
+    public void setBaseLocation(Location baseLocation) {
+        this.baseLocation = baseLocation;
     }
 
     public String getName() {
@@ -28,12 +62,16 @@ public class Team implements ConfigurationSerializable {
         return baseLocation;
     }
 
-    public Player getOwner() {
-        return Owner;
+    public UUID getOwner() {
+        return owner;
     }
 
-    public SurvivalPlayer[] getMembers() {
+    public List<UUID> getMembers() {
         return members;
+    }
+
+    public void addMember(UUID uuid) {
+        members.add(uuid);
     }
 
     @Override
@@ -41,8 +79,8 @@ public class Team implements ConfigurationSerializable {
         Map<String, Object> map = new HashMap<>();
 
         map.put("name", getName());
-        map.put("baseLocation", getBaseLocation().serialize());
-        map.put("owner", getOwner());
+        map.put("base", getBaseLocation().serialize());
+        map.put("owner", getOwner().toString());
         map.put("members", getMembers());
 
         return map;
