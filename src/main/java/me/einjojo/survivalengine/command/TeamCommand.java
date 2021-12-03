a8cf0e0bfe609129830b08ae8271995fe054fc85
Johannes / EinJojo <62117829+EinJOJO@users.noreply.github.com>
2021-12-03 11:54:34 +0100

    Info about online state

--- src/main/java/me/einjojo/survivalengine/command/TeamCommand.java
+++ src/main/java/me/einjojo/survivalengine/command/TeamCommand.java
@@ -102,7 +102,7 @@ public class TeamCommand implements CommandExecutor {
                 TextComponent textComponent = new TextComponent(String.format("%s§f%s §7» §bMitglieder §7(§3%d§7) \n", plugin.getPREFIX(), team.getName(), team.getMembers().size()));
                 for (UUID memberUUID : team.getMembers()) {
                     OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(memberUUID);
-                    textComponent.addExtra(" §8- §7" + offlinePlayer.getName() + "\n");
+                    textComponent.addExtra(String.format(" §8- §b%s §7(§s§7)\n", offlinePlayer.getName(), offlinePlayer.isOnline() ? "§aOnline": "§cOffline"));
                 }
                 player.spigot().sendMessage(textComponent);
                 return;
