71919dcdd7c1fae82f3b2621ce070079dfb2febd
Johannes / EinJojo <62117829+EinJOJO@users.noreply.github.com>
2021-12-16 11:23:12 +0100

    send help3

---
+++ src/main/java/me/einjojo/survivalengine/inventory/ChickenInventory.java
@@ -0,0 +1,18 @@
+package me.einjojo.survivalengine.inventory;
+
+
+import org.bukkit.inventory.Inventory;
+import me.einjojo.survivalengine.
+
+public class ChickenInventory {
+
+
+
+
+    public Inventory getInventory()
+
+    public Inventory getInventory(Inventory inventory) {
+
+    }
+
+}
