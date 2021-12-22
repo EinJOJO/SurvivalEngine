9eed086f0fe375c80dc064a5adc97e274190792a
Johannes / EinJojo <62117829+EinJOJO@users.noreply.github.com>
2021-12-23 00:40:29 +0100

    Code Refactor

--- src/main/java/me/einjojo/survivalengine/enchantment/TelepathyEnchantment.java
+++ src/main/java/me/einjojo/survivalengine/enchantment/TelepathyEnchantment.java
@@ -1,7 +1,6 @@
 package me.einjojo.survivalengine.enchantment;
 
 import me.einjojo.survivalengine.SurvivalEngine;
-import me.einjojo.survivalengine.manager.EnchantmentManager;
 import org.bukkit.GameMode;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
@@ -14,6 +13,7 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 public class TelepathyEnchantment extends EnchantmentWrapper implements Listener {
@@ -29,62 +29,44 @@ public class TelepathyEnchantment extends EnchantmentWrapper implements Listener
         ItemStack itemStack = player.getInventory().getItemInMainHand();
         Block block = e.getBlock();
 
-        if(itemStack.getItemMeta() == null) {
-            return;
-        }
-        if(!itemStack.getItemMeta().hasEnchant(this)){
-            return;
-        }
-
-        if(player.getGameMode() == GameMode.CREATIVE || player.getGameMode() == GameMode.SPECTATOR) {
-            return;
-        }
-        if(player.getInventory().firstEmpty() == -1) {
-            return;
-        }
-        if(block.getState() instanceof Container) {
-            return;
-        }
+        if(itemStack.getItemMeta() == null) return;
+        if(!itemStack.getItemMeta().hasEnchant(this)) return;
+        if(player.getGameMode() == GameMode.CREATIVE || player.getGameMode() == GameMode.SPECTATOR) return;
+        if(block.getState() instanceof Container) return;
         e.setDropItems(false);
 
         Collection<ItemStack> drops = e.getBlock().getDrops(itemStack, player);
-        if(drops.size() == 0) {
-            return;
-        }
-        player.getInventory().addItem(drops.iterator().next());
-        player.playSound(player.getLocation(), Sound.ENTITY_ITEM_PICKUP, 0.7f, 2);
+        handleDrops(player, drops);
     }
 
     @EventHandler
     public void onEntityKill(EntityDeathEvent e) {
         Player player = e.getEntity().getKiller();
-
-        if(player == null) {
-            return;
-        }
-
+        if(player == null) return;
         ItemStack itemStack = player.getInventory().getItemInMainHand();
-        if(itemStack.getItemMeta() == null) {
-            return;
-        }
-        if(!itemStack.getItemMeta().hasEnchant(this)){
-            return;
-        }
-
-        if(player.getInventory().firstEmpty() == -1) {
-            return;
-        }
-
+        if(itemStack.getItemMeta() == null) return;
+        if(!itemStack.getItemMeta().hasEnchant(this)) return;
         List<ItemStack> drops = e.getDrops();
-        if(drops.size() == 0) {
-            return;
-        }
-        player.getInventory().addItem(drops.iterator().next());
+        handleDrops(player, drops);
         drops.clear();
         player.setTotalExperience(player.getTotalExperience() + e.getDroppedExp());
-        player.playSound(player.getLocation(), Sound.ENTITY_ITEM_PICKUP, 0.7f, 2);
     }
 
+
+    private void handleDrops(Player receiver, Collection<ItemStack> drop) {
+        if(drop.size() == 0) {
+            return;
+        }
+        drop.forEach((item) -> {
+            HashMap<Integer, ItemStack> toDrop = receiver.getInventory().addItem(item);
+            if(toDrop.size() > 0) {
+                toDrop.forEach((number, lostItem) -> {
+                    receiver.getWorld().dropItem(receiver.getLocation(), lostItem);
+                });
+            }
+        });
+        receiver.playSound(receiver.getLocation(), Sound.ENTITY_ITEM_PICKUP, 0.7f, 2);
+    }
 
 
 }
